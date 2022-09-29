584e8d1788b471e32e042ae1e3dc3ca75144a303
Each Chen <iach526526@gmail.com>
2022-09-29 10:37:37 +0800

    ver2.0
    1.非字母不加密
    2.自行輸入位移量

--- caesar-cipher.cpp
+++ caesar-cipher.cpp
@@ -3,15 +3,24 @@
 int main() {
 	
 	char a[10];
+	int key;
+	printf("輸入位移量:");
+	scanf("%d",&key);
+	fflush(stdin);//清空緩衝區資料 
+	printf("%d",key);//位移量 
+	printf("輸入字母做加/解密"); 
 	while(scanf("%s",a)!=EOF)
 	{
 		int i=0;
 		while(a[i]!='\0'){
-			if(isupper(a[i])){
-				printf("%c",((a[i]-'A'+16)%26+'A'));
+			if(isupper(a[i])){//從字串逐一取出字元做位移 
+				printf("%c",((a[i]-'A'+key)%26+'A'));//取出在A~Z中的是第幾個字母，再加上位移量mod 26，避免超出26，最後把編碼加回去 
+			}
+			else if(islower(a[i])) {
+				printf("%c",((a[i]-'a'+key)%26+'a'));
 			}
 			else{
-				printf("%c",((a[i]-'a'+16)%26+'a'));
+				printf("不是字母");
 			}
 			i++;
 		}
